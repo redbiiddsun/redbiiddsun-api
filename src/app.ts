ce1553ba011cfd14857307bda69d6a32a86e7d88
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-02-15 03:15:54 +0700

    ✨ feat: add handle invalid json error

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import bodyParser from "body-parser";
+import bodyParserErrorHandler from "express-body-parser-error-handler";
 import "dotenv/config";
 
 import apiStatus from "./routes/status.routes";
+import userRoutes from "./routes/user.routes";
 
 export const app = express();
 
@@ -10,7 +12,17 @@ export const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+// Error handling for invalid JSON body
+app.use(
+    bodyParserErrorHandler({
+        errorMessage: () => {
+            return `Invalid JSON body`;
+        },
+    })
+);
+
 app.use("/api/status", apiStatus);
+app.use("/api/user", userRoutes);
 
 app.get("/", (req, res) => {
     res.send("Welcome to Redbiiddsun API! 🚀");
