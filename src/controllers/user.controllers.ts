bfd852d671618e84f5113b372d8c55dd6960b662
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-02-15 03:44:11 +0700

    ✨feat: add create user controller

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -1,6 +1,30 @@
-import type { Request, Response, NextFunction } from "express"
+import type { Request, Response, NextFunction } from "express";
+import { prisma } from "../lib/prisma";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
+import responseFormat from "../lib/responseFormat";
 
+export async function createUser(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    const userData = req.body;
 
-export async function createUser(req: Request, res: Response, next: NextFunction){
-    
+    try {
+        const createUser = await prisma.user.create({ data: { ...userData } });
+
+        res.status(201).json(
+            responseFormat(true, "create user successfully", createUser)
+        );
+    } catch (error) {
+        if (
+            error instanceof PrismaClientKnownRequestError &&
+            error.code === "P2002"
+        ) {
+            res.status(400).json(
+                responseFormat(false, "Email Already Exists", null)
+            );
+        }
+        res.status(500).json(responseFormat(false, "Internal Error", null));
+    }
 }
