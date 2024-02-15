0c3eb2bf67df730833c078f52f3c3fc96cfb8dc8
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-02-15 20:36:33 +0700

    ✨feat: implement a hashPassword and change insert data prisma format

--- src/controllers/user.controllers.ts
+++ src/controllers/user.controllers.ts
@@ -2,19 +2,32 @@ import type { Request, Response, NextFunction } from "express";
 import { prisma } from "../lib/prisma";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 import responseFormat from "../lib/responseFormat";
+import { hashPassword } from "../lib/hash";
+
 
 export async function createUser(
     req: Request,
     res: Response,
     next: NextFunction
 ) {
-    const userData = req.body;
+    const { username, email, password, first_name, last_name, role } = req.body;
+
+    let securePassword = await hashPassword(password);
 
     try {
-        const createdUser = await prisma.user.create({ data: { ...userData } });
+        const createdUser = await prisma.user.create({
+            data: {
+                username,
+                email,
+                password: securePassword,
+                first_name,
+                last_name,
+                role,
+            },
+        });
 
         // return data Exclude password
-        let {['password']: _, ...returnData} = createdUser;
+        let { ["password"]: _, ...returnData } = createdUser;
 
         res.status(201).json(
             responseFormat(true, "create user successfully", returnData)
@@ -27,7 +40,8 @@ export async function createUser(
             res.status(400).json(
                 responseFormat(false, "Email Already Exists", null)
             );
+        } else {
+            res.status(500).json(responseFormat(false, "Internal Error", null));
         }
-        res.status(500).json(responseFormat(false, "Internal Error", null));
     }
 }
