4514919f8f43e3f69d609d29b88b3ccc3cd48b9d
Redbiidd.sun <38393603+redbiiddsun@users.noreply.github.com>
2024-02-20 20:53:26 +0700

    ⚡️change: bodyParserErrorHandler to custom invalid json error

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,21 @@
-import express, { NextFunction } from "express";
-import bodyParserErrorHandler from "express-body-parser-error-handler";
+import express, {NextFunction, Request, Response } from "express";
 import "dotenv/config";
+import { handlerExpressParser } from "./lib/utils/error";
 
+
+// Route Import
 import apiStatus from "./routes/status.routes";
 import userRoutes from "./routes/user.routes";
+
 
 export const app = express();
 
 // Config Middleware for parsing JSON request bodies
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.raw())
+app.use(express.raw());
 
-
-// Error handling for invalid JSON body
-app.use(
-    bodyParserErrorHandler({
-        errorMessage: () => {
-            return `Invalid JSON body`;
-        },
-    })
-);
+app.use(handlerExpressParser);
 
 app.use("/api/status", apiStatus);
 app.use("/api/user", userRoutes);
